1cbccd0eeec6f15e34be6560f25441c9595a32f4
i-ice-bear <andydowsen47us@gmail.com>
2024-01-22 13:16:01 +0530

    Sanity CMS, initialised

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,15 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
-import { ClerkProvider } from '@clerk/nextjs'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { ClerkProvider } from "@clerk/nextjs";
+import NavbarComponent from "./components/Navbar";
 
-export default function AppComponent({Component, pageProps}:AppProps){
+export default function AppComponent({ Component, pageProps }: AppProps) {
   return (
     <ClerkProvider {...pageProps}>
-      <Component {...pageProps}/>
+      <div>
+        <NavbarComponent />
+        <Component {...pageProps} />
+      </div>
     </ClerkProvider>
-  )
+  );
 }
---
+++ src/pages/components/Navbar.tsx
@@ -0,0 +1,11 @@
+// import Reaect from "react";
+
+// const NavbarComponent = () => {
+//   return (
+//     <>
+//       <h1>Routable component, Initialised!~</h1>
+//     </>
+//   );
+// };
+
+// export default NavbarComponent;
